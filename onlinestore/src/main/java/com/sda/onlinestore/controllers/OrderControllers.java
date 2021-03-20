0c7de696e8faa71b1f8317f5683e18385b9d1c9c
Campean Adrian <adriancampean07@gmail.com>
2021-03-20 13:58:17 +0200

    bootstrap update

--- onlinestore/src/main/java/com/sda/onlinestore/controllers/OrderControllers.java
+++ onlinestore/src/main/java/com/sda/onlinestore/controllers/OrderControllers.java
@@ -1,17 +1,21 @@
 package com.sda.onlinestore.controllers;
 
 import com.sda.onlinestore.entities.OrderEntity;
+import com.sda.onlinestore.entities.ProductEntity;
 import com.sda.onlinestore.servicies.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 import java.util.List;
 
 @Controller
-public class OderControllers {
+public class OrderControllers {
 
     @Autowired
     private OrderService orderService;
